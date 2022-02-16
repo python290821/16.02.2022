74f74cc0bff6c01a1796c2f1fa11af9fd484ae56
micserv010821 <dotnet290320@gmail.com>
2022-02-16 19:44:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,45 @@
-import threading
+from threading import Semaphore, Thread, Event
+import json
+import time
+import sys
 import time
 import logging
 
 logging.basicConfig(level=logging.DEBUG,
-                    format='(%(threadName)-9s) %(message)s %(process)s', )
-exitFlag = 0
+                    format='(%(threadName)-9s) %(message)s', )
 
-class myThread (threading.Thread):
-   def __init__(self, threadID, name, counter):
-      threading.Thread.__init__(self)
-      self.threadID = threadID
-      self.name = name
-      self.counter = counter
-   def run(self):
-      logging.debug("Starting " + self.name)
-      logging.debug('sleeping for 5 seconds')
-      time.sleep(315)
-      logging.debug ("Exiting " + self.name)
+def wait_for_event(e):
+    logging.debug('wait_for_event starting')
+    event_is_set = e.wait()
+    logging.debug('event set: %s', event_is_set)
 
-# Create new threads
-thread1 = myThread(1, "Thread-1", 1)
-thread1.start()
 
-logging.debug('waiting for thread 1')
+def wait_for_event_timeout(e, t):
+    while not e.isSet():
+        logging.debug('wait_for_event_timeout starting')
+        event_is_set = e.wait(2)
+        logging.debug('event set: %s', event_is_set)
+        if event_is_set:
+            logging.debug('processing event')
+        else:
+            logging.debug('doing other things')
 
-# here in join thread main is active waiting, heavy performance
-# current_thread_wait_for(thread1)
-thread1.join() # wait for thread 1 to finish
+if __name__ == '__main__':
+    e = Event()
+    t1 = Thread(name='blocking',
+                          target=wait_for_event,
+                          args=(e,))
+    t1.start()
 
-logging.debug('thread 1 finished')
-logging.debug('done')
+    t2 = Thread(name='non-blocking',
+                          target=wait_for_event_timeout,
+                          args=(e, 2000))
+    t2.start()
 
-# loading table
-# new_thread -> bring data
-# print inside table : "waiting for data ..."
-# join (new_thread, 3 seconds)
-# print inside table : "slow internet connection ..."
+    logging.debug('Waiting before calling Event.set()')
+    time.sleep(10)
+    e.set()
+    logging.debug('Event is set')
 
-# thread_go_play
-# join thread(60 * 60) -- wait for thread to finish
-# drive home
+
+sys.exit(0)
